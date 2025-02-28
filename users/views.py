6ecb6cc8ee84ead029118ea0780c55a6f623991b
juan sebastian ospina losada <juan.ospina@unp.gov.co>
2025-02-28 11:26:48 -0500

    refactor(users): Modificacion del user_token para agregar rutas

--- users/views.py
+++ users/views.py
@@ -20,16 +20,39 @@ def get_data(request):
             if not basicos_usuario:
                 return JsonResponse({'message': 'Required basic data', 'BdRequired': True}, status=status.HTTP_200_OK)
 
+            perfiles_usr = PerfilUsuario.objects.filter(
+                basicos_usuario=basicos_usuario)
+            
+            urls_usuario = []
+            for perfil_usr in perfiles_usr:
+                url = f'{perfil_usr.dependencia.url}/{perfil_usr.grupo.url}/{perfil_usr.rol.url}'
+                urls_usuario.append(url)
+
             datos_basicos = {
                 "dependencia": basicos_usuario.dependencia.nombre_dependencia,
                 "grupo": basicos_usuario.grupo.nombre_grupo,
-                "rol": basicos_usuario.rol.nombre_rol
+                "rol": [perfil_usr.rol.nombre_rol for perfil_usr in perfiles_usr],
+                "rutas": urls_usuario
             }
 
             user_token = generate_jwt_token(data={'datos_basicos': datos_basicos, 'address': request.META.get(
                 'REMOTE_ADDR'), 'type': 'data', 'user_type': 'external'}, exp_minutes=60)
 
-            return JsonResponse({'message': 'Data retrieved', 'user_token': user_token}, status=status.HTTP_200_OK)
+            response = JsonResponse(
+                {'message': 'Data retrieved'}, status=status.HTTP_200_OK)
+
+            response.set_cookie(
+                key='user_token',
+                value=user_token,
+                max_age=60 * 60,
+                expires=1,
+                secure=False,
+                httponly=True,
+                samesite='Lax',
+                path='/'
+            )
+
+            return response
         except Exception as e:
             return JsonResponse({'message': f'Error en la data {e}'}, status=status.HTTP_400_BAD_REQUEST)
 
